67a13f9183e52d774353012bfda58c360f1d7e51
nebunebu <nebu.nebuchadnezzar@gmail.com>
2024-08-17 21:01:53 -0400

    this works

--- flake.nix
+++ flake.nix
@@ -1,61 +1,12 @@
 {
   description = "A collection of development environments";
 
-
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-  };
-
-  outputs = inputs:
-    let
-      supportedSystems = [
-        "x86_64-linux"
-        "aarch64-linux"
-        "x86_64-darwin"
-        "aarch64-darwin"
-      ];
-      forAllSystems = inputs.nixpkgs.lib.genAttrs supportedSystems;
-    in
+  outputs = _:
     {
-      # packages = forAllSystems
-      #   (system:
-      #     let
-      #       pkgs = inputs.nixpkgs.legacyPackages.${system};
-      #     in
-      #     {
-      #       luaBundle = pkgs: [
-      #         pkgs.lua-language-server
-      #         pkgs.stylua
-      #         pkgs.luajitPackages.luacheck
-      #       ];
-      #
-      #       jsBundle = pkgs: [
-      #         pkgs.prettierd
-      #         pkgs.stylelint
-      #       ];
-      #     }
-      #   );
-
-      devShells = forAllSystems (
-        system:
-        let
-          pkgs = inputs.nixpkgs.legacyPackages.${system};
-        in
-        {
-          default = pkgs.mkShell {
-            name = "luaShell";
-            packages = builtins.attrValues {
-
-              inherit (pkgs)
-                lua-language-server
-                stylua
-                ;
-              inherit (pkgs.luajitPackages)
-                luacheck
-                ;
-            };
-          };
-        }
-      );
+      lib = {
+        hello = pkgs: with pkgs; [ cowsay hello ];
+        lua = pkgs: with pkgs; [ lua-language-server stylua luajitPackages.luacheck ];
+        nix = pkgs: with pkgs; [ nixd deadnix statix nixfmt-rfc-style ];
+      };
     };
 }
