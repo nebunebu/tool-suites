9143576ea6cbea46cf3bcbc82c5052bc4c907bbb
nebunebu <nebu.nebuchadnezzar@gmail.com>
2024-08-17 22:20:01 -0400

    add some pkg bundles

--- flake.nix
+++ flake.nix
@@ -4,9 +4,15 @@
   outputs = _:
     {
       lib = {
+        css = pkgs: with pkgs; [ prettierd ];
         hello = pkgs: with pkgs; [ cowsay hello ];
+        js = pkgs: with pkgs; [ prettierd ];
         lua = pkgs: with pkgs; [ lua-language-server stylua luajitPackages.luacheck ];
+        md = pkgs: with pkgs; [ markdownlint ];
         nix = pkgs: with pkgs; [ nixd deadnix statix nixfmt-rfc-style ];
+        scss = pkgs: with pkgs; [ prettierd ];
+        tex = pkgs: with pkgs; [ latexindent chktex ];
+        ts = pkgs: with pkgs; [ prettierd ];
       };
     };
 }
