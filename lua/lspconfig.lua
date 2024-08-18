00d467e551ed690af36078b1e14ae6440984a2ea
nebunebu <nebu.nebuchadnezzar@gmail.com>
2024-08-18 17:06:19 -0400

    start to set up lua/

---
+++ lua/lspconfig.lua
@@ -0,0 +1,9 @@
+require("lspconfig").bashls.setup({})
+require("lspconfig").cssls.setup({}) -- css, scss, less
+-- require("lspconfig").eslint.setup({}) -- js, ts
+-- require("lspconfig").jqls.setup({})
+-- require("lspconfig").jsonls.setup({})
+require("lspconfig").lua_ls.setup({})
+require("lspconfig").nixd.setup({})
+require("lspconfig").texlab.setup({}) -- latex
+require("lspconfig").yamlls.setup({})
