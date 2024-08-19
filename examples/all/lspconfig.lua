482f23fb4d0dd8ed84f0f559685a9bd46e4cda83
nebunebu <nebu.nebuchadnezzar@gmail.com>
2024-08-18 20:12:31 -0400

    restructure files

--- examples/all/lspconfig.lua
+++
@@ -1,9 +0,0 @@
--- require("lspconfig").bashls.setup({})
--- require("lspconfig").cssls.setup({}) -- css, scss, less
--- require("lspconfig").eslint.setup({}) -- js, ts
--- require("lspconfig").jqls.setup({})
--- require("lspconfig").jsonls.setup({})
-require("lspconfig").lua_ls.setup({})
--- require("lspconfig").nixd.setup({})
--- require("lspconfig").texlab.setup({}) -- latex
--- require("lspconfig").yamlls.setup({})
