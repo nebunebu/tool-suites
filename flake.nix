9a2123496f60c4dc245b455c827cc40d74aea018
nebunebu <nebu.nebuchadnezzar@gmail.com>
2024-08-19 19:26:26 -0400

    make mkToolSuite overridable

--- flake.nix
+++ flake.nix
@@ -19,61 +19,82 @@
       ];
     in
     {
-      lib = rec {
-        mkToolSuite = { lang ? null, lsps ? [ ], linters ? [ ], formatters ? [ ], other ? [ ] }:
-          builtins.concatLists [ (inputs.nixpkgs.lib.optionals (lang != null) [ lang ]) lsps linters formatters other ];
-        bash = pkgs: mkToolSuite {
-          lang = pkgs.bash;
-          lsps = [ pkgs.nodePackages.bash-language-server ];
-          linters = [ pkgs.shellcheck ];
-          formatters = [ pkgs.shfmt ];
-        };
+      lib = forAllSystems (
+        system:
+        let
+          pkgs = inputs.nixpkgs.legacyPackages.${system};
+        in
+        rec {
+          mkToolSuite = pkgs.lib.makeOverridable (
+            { lang ? null
+            , lsps ? [ ]
+            , linters ? [ ]
+            , formatters ? [ ]
+            , other ? [ ]
+            }:
+            builtins.concatLists
+              [
+                (inputs.nixpkgs.lib.optionals (lang != null) [ lang ])
+                lsps
+                linters
+                formatters
+                other
+              ]
+          );
 
-        json = pkgs: mkToolSuite {
-          # lsps = [ pkgs.lua-language-server ];
-          linters = [ pkgs.nodePackages.jsonlint ];
-          # formatters = [ pkgs.stylua ];
-        };
+          bash = pkgs: mkToolSuite {
+            lang = pkgs.bash;
+            lsps = [ pkgs.nodePackages.bash-language-server ];
+            linters = [ pkgs.shellcheck ];
+            formatters = [ pkgs.shfmt ];
+          };
 
-        lua = pkgs: mkToolSuite {
-          lang = pkgs.lua;
-          lsps = [ pkgs.lua-language-server ];
-          linters = [ pkgs.luajitPackages.luacheck ];
-          formatters = [ pkgs.stylua ];
-        };
+          json = pkgs: mkToolSuite {
+            # lsps = [ pkgs.lua-language-server ];
+            linters = [ pkgs.nodePackages.jsonlint ];
+            # formatters = [ pkgs.stylua ];
+          };
 
-        latex = pkgs: mkToolSuite {
-          lsps = [ pkgs.texlab ];
-          linters = [ pkgs.texlivePackages.chktex ];
-          formatters = [ pkgs.texlivePackages.latexindent ];
-        };
+          lua = pkgs: mkToolSuite {
+            lang = pkgs.lua;
+            lsps = [ pkgs.lua-language-server ];
+            linters = [ pkgs.luajitPackages.luacheck ];
+            formatters = [ pkgs.stylua ];
+          };
 
-        nix = pkgs: mkToolSuite {
-          lsps = [ pkgs.nixd ];
-          linters = [ pkgs.statix pkgs.deadnix ];
-          formatters = [ pkgs.nixfmt-rfc-style ];
-          # other = [ pkgs.nixdoc ];
-        };
+          latex = pkgs: mkToolSuite {
+            lsps = [ pkgs.texlab ];
+            linters = [ pkgs.texlivePackages.chktex ];
+            formatters = [ pkgs.texlivePackages.latexindent ];
+          };
 
-        # ocaml = pkgs: mkToolSuite {
-        #   lang = pkgs.ocaml;
-        #   lsps = [ pkgs.ocamlPackages.ocaml-lsp ];
-        #   formatters = [ pkgs.ocamlPackages.ocamlformat pkgs.ocamlPackages.ocp-indent ];
-        #   other = [ pkgs.opam pkgs.ocamlPackages.utop ];
-        # };
+          nix = pkgs: mkToolSuite {
+            lsps = [ pkgs.nixd ];
+            linters = [ pkgs.statix pkgs.deadnix ];
+            formatters = [ pkgs.nixfmt-rfc-style ];
+            # other = [ pkgs.nixdoc ];
+          };
 
-        xml = pkgs: mkToolSuite {
-          lsps = [ pkgs.lemminx ];
-          linters = [ pkgs.libxml2 ]; # provides xmllint
-          formatters = [ pkgs.xmlformat ];
-        };
+          # ocaml = pkgs: mkToolSuite {
+          #   lang = pkgs.ocaml;
+          #   lsps = [ pkgs.ocamlPackages.ocaml-lsp ];
+          #   formatters = [ pkgs.ocamlPackages.ocamlformat pkgs.ocamlPackages.ocp-indent ];
+          #   other = [ pkgs.opam pkgs.ocamlPackages.utop ];
+          # };
 
-        yaml = pkgs: mkToolSuite {
-          lsps = [ pkgs.yaml-language-server ];
-          linters = [ pkgs.yamllint ];
-          formatters = [ pkgs.yamlfmt ];
-        };
-      };
+          xml = pkgs: mkToolSuite {
+            lsps = [ pkgs.lemminx ];
+            linters = [ pkgs.libxml2 ]; # provides xmllint
+            formatters = [ pkgs.xmlformat ];
+          };
+
+          yaml = pkgs: mkToolSuite {
+            lsps = [ pkgs.yaml-language-server ];
+            linters = [ pkgs.yamllint ];
+            formatters = [ pkgs.yamlfmt ];
+          };
+        }
+      );
 
       checks = forAllSystems (
         system:
@@ -107,13 +128,12 @@
           default = pkgs.mkShell {
             name = "dev-environments";
             packages = [
-              (inputs.self.lib.lua pkgs)
-              (inputs.self.lib.nix pkgs)
+              (inputs.self.lib.${system}.lua pkgs)
+              (inputs.self.lib.${system}.nix pkgs)
               # (inputs.self.lib.git pkgs)
             ];
           };
         }
       );
-
     };
 }
