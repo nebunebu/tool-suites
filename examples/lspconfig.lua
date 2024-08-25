b3d0c8000b029a1532f38fa349a01ef0599292bd
nebunebu <nebu.nebuchadnezzar@gmail.com>
2024-08-24 21:01:18 -0400

    remove examples/, test/

--- examples/lspconfig.lua
+++
@@ -1,6 +0,0 @@
-require("lspconfig").bashls.setup({})
-require("lspconfig").lua_ls.setup({})
-require("lspconfig").nixd.setup({})
-require("lspconfig").texlab.setup({})
-require("lspconfig").lemminx.setup({}) --xml
-require("lspconfig").yamlls.setup({}) --xml
--- examples/nvim-lint.lua
+++
@@ -1,8 +0,0 @@
-require("lint").linters_by_ft = {
-	bash = { "shellcheck" },
-	lua = { "luacheck" },
-	nix = { "deadnix", "statix" },
-	tex = { "chktex" },
-	xml = { "xmllint" },
-	yaml = { "yamllint" },
-}
